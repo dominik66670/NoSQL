e7865687d527c8c99be72ad5b1693f6a7e6573c8
Dominik Filipczuk <dominik66670@hotmail.com>
2025-01-14 11:15:34 +0100

    Init commit

--- NoSQL/Models/Book.cs
+++ NoSQL/Models/Book.cs
@@ -4,6 +4,7 @@ namespace NoSQL.Models
 {
     public class Book
     {
+        //aaa
         public int id { get; set; }
         public string isbn { get; set; }
         public string opis { get; set; }
